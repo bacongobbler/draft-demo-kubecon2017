00dd3867f2a7d60c204043519bf35df5f5b03dd7
Matthew Fisher <matt.fisher@microsoft.com>
2017-11-21 11:58:13 -0800

    login to registry before pushing

--- brigade.js
+++ brigade.js
@@ -25,15 +25,18 @@ events.on("push", (e, p) => {
     if (payload.ref == "refs/heads/master") {
       var buildJob = new Job("docker-build")
       buildJob.env = {
-        "IMAGE_NAME": [p.secrets.DOCKER_REGISTRY, "draft-demo-kubecon2017"].join("/")
+        "IMAGE_NAME": [p.secrets.DOCKER_REGISTRY, "draft-demo-kubecon2017"].join("/"),
+        "DOCKER_REGISTRY": p.secrets.DOCKER_REGISTRY,
+        "DOCKER_USER": p.secrets.DOCKER_USER,
+        "DOCKER_PASS": p.secrets.DOCKER_PASS,
       }
 
       buildJob.image = "docker:17.06.0-ce"
       buildJob.docker.enabled = true
       buildJob.tasks = [
         "cd /src",
-        "IMAGE_ID=$(docker build --quiet .)",
-        "docker tag $IMAGE_ID $IMAGE_NAME",
+        "docker login --username $DOCKER_USER --password $DOCKER_PASS $DOCKER_REGISTRY",
+        "docker build -t $IMAGE_NAME .",
         "docker push $IMAGE_NAME"
       ]
       buildJob.run()
