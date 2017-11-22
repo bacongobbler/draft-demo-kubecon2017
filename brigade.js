0dc633e592f4af5fabacc9b12024f19f03c60217
Matthew Fisher <matt.fisher@microsoft.com>
2017-11-22 11:31:07 -0800

    remove loadbalancer type

--- brigade.js
+++ brigade.js
@@ -69,7 +69,7 @@ events.on("imagePush", (e, p) => {
   var helm = new Job("helm", "lachlanevenson/k8s-helm:" + helmTag)
   helm.tasks = [
     "ls /src",
-    "helm upgrade --reuse-values --set image.tag='" + version + "',image.repository='" + imageName + "',service.type='Loadbalancer' --install " + name + " /src/charts/uuid-generator"
+    "helm upgrade --reuse-values --set image.tag='" + version + "',image.repository='" + imageName + "' --install " + name + " /src/charts/uuid-generator"
   ]
 
   helm.run().then( result => {
